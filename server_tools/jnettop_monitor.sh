ea0ee2a841824b179529de01aa08d30a3a8787c6
Christophe-Foyer <christophe.aa.foyer@gmail.com>
2024-03-03 20:51:11 +0000

    Full paths

--- server_tools/jnettop_monitor.sh
+++ server_tools/jnettop_monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Edit the variables below
-suspend_command="shutdown"
+suspend_command="/usr/bin/systemctl hibernate"
 scantime=300 # How long of a period should each scan scan
 ports='80 443' # Ports to monitor
 min_uptime_minutes=10  # Minimum uptime in minutes
@@ -12,15 +12,15 @@ upSeconds="$(/usr/bin/cat /proc/uptime | /usr/bin/grep -o '^[0-9]\+')"
 upMins=$((${upSeconds} / 60))
 
 # Aggregate the rules to look for
-scanrules=$(echo $(for port in $ports; do echo -n "dstport:$port|"; done)  | rev | cut -c 2- | rev)
+scanrules=$(/usr/bin/echo $(for port in $ports; do echo -n "dstport:$port|"; done)  | /usr/bin/rev | /usr/bin/cut -c 2- | /usr/bin/rev)
 echo "Grep matching rules: $scanrules"
 
 # Scan network activity
-ret=$(/usr/sbin/jnettop --display text -t $scantime --format 'proto:$proto$,src:$srcname$,dstport:$dstport$,bps:$totalbps$' | grep -E "$scanrules")
+ret=$(/usr/sbin/jnettop --display text -t $scantime --format 'proto:$proto$,src:$srcname$,dstport:$dstport$,bps:$totalbps$' | /usr/bin/grep -E "$scanrules")
 
 # Check x session idle time
-get_idle_time=`dbus-send --print-reply --dest=org.gnome.Mutter.IdleMonitor /org/gnome/Mutter/IdleMonitor/Core org.gnome.Mutter.IdleMonitor.GetIdletime`
-idle_time=$(echo $get_idle_time | awk '{print $NF}')  # milliseconds
+get_idle_time=`/usr/bin/dbus-send --print-reply --dest=org.gnome.Mutter.IdleMonitor /org/gnome/Mutter/IdleMonitor/Core org.gnome.Mutter.IdleMonitor.GetIdletime`
+idle_time=$(/usr/bin/echo $get_idle_time | /usr/bin/awk '{print $NF}')  # milliseconds
 
 # Run suspend_command
 if [ -z "$ret" ] && [ "${upMins}" -gt "${min_minutes}" ] && [ "$idle_time" -gt $(( min_idle_time_minutes*60*1000 )) ]
