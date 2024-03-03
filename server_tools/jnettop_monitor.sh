dc629e711348770e7b91a9e49baed186357b627a
Christophe-Foyer <christophe.aa.foyer@gmail.com>
2024-03-03 20:44:50 +0000

    Script is now happy I think

--- server_tools/jnettop_monitor.sh
+++ server_tools/jnettop_monitor.sh
@@ -2,10 +2,10 @@
 
 # Edit the variables below
 suspend_command="shutdown"
-cancel_command="shutdown -c 'Monitored application is using bandwidth'"
-scantime=60 # How long of a period should each scan scan
+scantime=300 # How long of a period should each scan scan
 ports='80 443' # Ports to monitor
 min_uptime_minutes=10  # Minimum uptime in minutes
+min_idle_time_minutes=10  # minimum idle time
 
 # Uptime
 upSeconds="$(/usr/bin/cat /proc/uptime | /usr/bin/grep -o '^[0-9]\+')"
@@ -18,5 +18,12 @@ echo "Grep matching rules: $scanrules"
 # Scan network activity
 ret=$(/usr/sbin/jnettop --display text -t $scantime --format 'proto:$proto$,src:$srcname$,dstport:$dstport$,bps:$totalbps$' | grep -E "$scanrules")
 
-# Suspend_command
-echo ret
+# Check x session idle time
+get_idle_time=`dbus-send --print-reply --dest=org.gnome.Mutter.IdleMonitor /org/gnome/Mutter/IdleMonitor/Core org.gnome.Mutter.IdleMonitor.GetIdletime`
+idle_time=$(echo $get_idle_time | awk '{print $NF}')  # milliseconds
+
+# Run suspend_command
+if [ -z "$ret" ] && [ "${upMins}" -gt "${min_minutes}" ] && [ "$idle_time" -gt $(( min_idle_time_minutes*60*1000 )) ]
+then
+    eval "$suspend_command"
+fi
