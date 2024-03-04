600bc9a77a14c54cadf60d3cf448146d02da65e8
Christophe-Foyer <christophe.aa.foyer@gmail.com>
2024-03-04 09:04:57 +0000

    Silly issue

--- app.py
+++ app.py
@@ -81,24 +81,24 @@ def wake_if_off_route(func):
     return wake_if_off
 
 
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>',methods=['GET','POST','DELETE'])
 @wake_if_off_route
 def proxy(path):
-    global SITE_NAME
     if request.method=='GET':
-        resp = requests.get(f'{SITE_NAME}{path}')
+        resp = requests.get(f'{url}{path}')
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
         return response
     elif request.method=='POST':
-        resp = requests.post(f'{SITE_NAME}{path}',json=request.get_json())
+        resp = requests.post(f'{url}{path}',json=request.get_json())
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
         return response
     elif request.method=='DELETE':
-        resp = requests.delete(f'{SITE_NAME}{path}').content
+        resp = requests.delete(f'{url}{path}').content
         response = Response(resp.content, resp.status_code, headers)
         return response
 
