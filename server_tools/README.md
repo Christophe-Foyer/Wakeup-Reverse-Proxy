ddd63b4a3d1e12567e01264d62719c13b8d87ad9
Christophe-Foyer <christophe.aa.foyer@gmail.com>
2024-03-13 11:44:43 +0000

    hackish workaround that works

--- server_tools/README.md
+++ server_tools/README.md
@@ -22,6 +22,16 @@ Relies on jnettop (`sudo apt install jnettop` on ubuntu).
 Must be run as the local user, and the `jnettop_sudoers` file must be added to `/etc/sudoers.d/jnettop_sudoers`. 
 This is a bit hackish but if you check what's in that file it should be pretty safe if you trust jnettop.
 
+You'll also need local ssh set up using keys to run the idle check command:
+```
+1. ssh-keygen -t rsa
+Press enter for each line 
+2. cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+3. chmod og-wx ~/.ssh/authorized_keys 
+```
+
+make sure you change the variables in the script to suit your needs (namely, the USER variable)
+
 Add this to crontab (`crontab -e`) to make this a recuring task (every 5 minutes here)
 ```
 */5 * * * * /bin/bash /path/to/scripts/jnettop_monitor.sh
--- server_tools/jnettop_monitor.sh
+++ server_tools/jnettop_monitor.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Edit the variables below
-suspend_command="sudo /usr/bin/systemctl hibernate"
+suspend_command="sudo /usr/bin/systemctl shutdown" # need to find a better way to handle hibernate
 scantime=300 # How long of a period should each scan scan
 ports='22 80 443' # Ports to monitor
 min_uptime_minutes=10  # Minimum uptime in minutes
 min_idle_time_minutes=10  # minimum idle time
+user="${USER:-root}"
 
 # Uptime
 upSeconds="$(/usr/bin/cat /proc/uptime | /usr/bin/grep -o '^[0-9]\+')"
@@ -19,19 +20,20 @@ echo "Grep matching rules: $scanrules"
 ret=$(sudo /usr/sbin/jnettop --display text -t $scantime --format 'proto:$proto$,src:$srcname$,srcport:$srcport$,dst:$dstname$,dstport:$dstport$,bps:$totalbps$' | /usr/bin/grep -E "$scanrules")
 echo "$ret"
 
+# Idle time is a mess to grab, but with the hackish ssh workaround it works
 # Check x session idle time
-get_idle_time=`/usr/bin/dbus-send --print-reply --dest=org.gnome.Mutter.IdleMonitor /org/gnome/Mutter/IdleMonitor/Core org.gnome.Mutter.IdleMonitor.GetIdletime`
+get_idle_time=`ssh -Y ${user}@:: /usr/bin/dbus-send --print-reply --dest=org.gnome.Mutter.IdleMonitor /org/gnome/Mutter/IdleMonitor/Core org.gnome.Mutter.IdleMonitor.GetIdletime`
 idle_time=$(/usr/bin/echo $get_idle_time | /usr/bin/awk '{print $NF}')  # milliseconds
 idle_time=$(( idle_time ))
 min_idle_time=$(( min_idle_time_minutes*60*1000 ))
 echo "idle_time: $idle_time (min: $min_idle_time)"
 
 # Run suspend_command
-if [[ -z "$ret" ]] && [[ "${upMins}" -gt "${min_minutes}" ]] && [[ "${idle_time}" -gt "${min_idle_time}" ]]
+if [[ -z "$ret" ]] && [[ "${upMins}" -gt "${min_uptime_minutes}" ]] # && [[ "${idle_time}" -gt "${min_idle_time}" ]]
 then
     eval "$suspend_command"
 else
-    echo "Idle time (min): $idle_time"
+    echo "Idle time (ms): $idle_time"
     echo "Detected network sessions:"
     echo "$ret"
 fi
